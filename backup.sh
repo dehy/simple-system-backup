11d7a41602aa774a3ad17d82351d235b634c046b
Arnaud de Mouhy <arnaud@admds.net>
2021-04-26 22:11:08 +0200

    fix: sh compatibility

--- backup.sh
+++ backup.sh
@@ -5,17 +5,17 @@ set -eux
 CURRENT_DIR=$(dirname $(readlink -f $0))
 RESTIC_BIN=$(which restic)
 
-source "$CURRENT_DIR/backuprc"
+. "$CURRENT_DIR/backuprc"
 
 export RESTIC_REPOSITORY="${RESTIC_REPOSITORY_PREFIX}/${BACKUPED_HOST}"
 
-if [ "${1:-}" == "--init" ]; then
+if [ "${1:-}" = "--init" ]; then
     # Init repository
     $RESTIC_BIN init
     exit 0
 fi
 
-if [ "${1:-}" == "--unlock" ]; then
+if [ "${1:-}" = "--unlock" ]; then
     # Unlock repository
     $RESTIC_BIN unlock
     exit 0
